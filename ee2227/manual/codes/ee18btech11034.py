ea2290ac734fc7d3a5a43ebc48ece871306f789a
adi2000pedavegi <adi2000pedavegi@gmail.com>
2020-04-18 12:53:11 +0530

    ee2227

--- ee2227/manual/codes/ee18btech11034.py
+++ ee2227/manual/codes/ee18btech11034.py
@@ -1,8 +1,15 @@
+#Code by P.Aditya
+#April 18th,2020
+#Released under GNU GPL
+
 import control
 import matplotlib.pyplot as plt
+import subprocess
+import shlex
 
-num = [-1,1]
-den = [2,4]
+
+num = [-1,1]  #numerator coefficients of G(s)
+den = [2,4]	  #denominator coefficients of G(s)
 
 #Creating a transfer function G = num/den
 G = control.tf(num,den) 
@@ -13,4 +20,5 @@ plt.grid(True)
 plt.title('Nyquist Diagram of G(s)')
 plt.xlabel('Re(s)')
 plt.ylabel('Im(s)')
-plt.show()
+plt.savefig('./figs/ee18btech11034.eps')
+#plt.show()
