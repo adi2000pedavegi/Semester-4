f047b10ea6b5169ccc12faebcdc5d3c85462b1dd
adi2000pedavegi <adi2000pedavegi@gmail.com>
2020-05-08 14:41:16 +0530

    updated manual

---
+++ ee2227/presentation/nyquist.py
@@ -0,0 +1,15 @@
+import control
+import matplotlib.pyplot as plt
+
+num = [-1,1]
+den = [2,4]
+
+#Creating a transfer function G = num/den
+G = control.tf(num,den) 
+
+control.nyquist(G)
+plt.grid(True)
+plt.title('Nyquist Diagram of G(s)')
+plt.xlabel('Re(s)')
+plt.ylabel('Im(s)')
+plt.show()
