217cc0ba29b1c33345ca7794ee5e33461730449d
adi2000pedavegi <adi2000pedavegi@gmail.com>
2020-05-12 20:22:29 +0530

    updated manual

---
+++ ee2227/ee18btech11034/spice/ee18btech11034_spice_result2.py
@@ -0,0 +1,26 @@
+#Code by P.ADITYA
+#Date 12th May,2020
+
+import numpy as np  
+import matplotlib.pyplot as plt
+
+
+#if using termux
+import subprocess
+import shlex
+#end if
+
+data=np.loadtxt('ee18btech11034_2.dat')  
+plt.plot(data[:,0],data[:,1])  
+plt.grid()
+plt.xlabel("time")
+plt.ylabel("Closed loop system magnitude response for $PM=45$")
+plt.title('Spice simulation')
+
+
+#if using termux
+plt.savefig('./figs/ee18btech11034/ee18btech11034_spice_result2.pdf')
+plt.savefig('./figs/ee18btech11034/ee18btech11034_spice_result2.eps')
+subprocess.run(shlex.split("termux-open ./figs/ee18btech11034/ee18btech11034_spice_result2.pdf"))
+#else
+#plt.show()
